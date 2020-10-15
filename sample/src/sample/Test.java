e0b65a1687c6551e5a492fa3845e671bb8c56412
pravalika <pravalika.kankara@ggktech.com>
2020-10-15 22:00:40 +0530

    Test

---
+++ sample/src/sample/Test.java
@@ -0,0 +1,23 @@
+package sample;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.gecko.driver","E:\\SeleniumPractice\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.co.in/");
+		int a = 2,b=3,c=0;
+		c = a + b;
+		System.out.println(c);
+		driver.close();
+		
+				
+	}
+
+}
